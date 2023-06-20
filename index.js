390e102e31a70317c088d999878963ccd0d96f7b
WinnySigilai <winnysigilai02@gmail.com>
2023-06-20 17:46:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Winny Sigilai';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
